0fb87fb471568ad7489782d72ae8efaefaf422e5
VitorBarbosa1 <VITORBARBOSA1301@GMAIL.COM>
2024-04-05 01:38:25 +0100

    Add Nav Styling

---
+++ services/urls.py
@@ -0,0 +1,12 @@
+# services/urls.py
+
+from django.urls import path
+from . import views
+
+app_name = 'services'
+
+urlpatterns = [
+ #   path('', views.service_list, name='list'),
+ #   path('search/', views.service_search, name='search'),
+
+]
